522965b269fcbfaca40043d40c12f935eb8774fa
Hafran Simbolon <haikal.123450106@student.itera.ac.id>
2025-12-15 23:38:46 +0700

    Update 05_Non-Clustered Indexes.sql

--- TUGAS BESAR DW/MISI 2/SQL Script/05_Non-Clustered Indexes.sql
+++ TUGAS BESAR DW/MISI 2/SQL Script/05_Non-Clustered Indexes.sql
@@ -1,10 +1,13 @@
--- Index untuk foreign keys (join optimization)
-CREATE NONCLUSTERED INDEX IX_Fact_Partisipasi_Kegiatan
-ON dbo.Fact_Partisipasi_Kegiatan(Kegiatan_SK)
-INCLUDE (Mahasiswa_SK);
-GO
-
-CREATE NONCLUSTERED INDEX IX_Fact_Dana_Kegiatan
-ON dbo.Fact_Dana_Kegiatan(Kegiatan_SK)
-INCLUDE (Jumlah_Pengajuan, Jumlah_Realisasi);
-GO
+-- Index untuk foreign keys (join optimization) 
+CREATE NONCLUSTERED INDEX IX_Fact_Partisipasi_Mahasiswa 
+ON dbo.Fact_Partisipasi_Kegiatan(Mahasiswa_SK) 
+INCLUDE (Kegiatan_SK); 
+GO 
+CREATE NONCLUSTERED INDEX IX_Fact_Partisipasi_Kegiatan 
+ON dbo.Fact_Partisipasi_Kegiatan(Kegiatan_SK) 
+INCLUDE (Mahasiswa_SK); 
+GO 
+CREATE NONCLUSTERED INDEX IX_Fact_Dana_Kegiatan 
+ON dbo.Fact_Dana_Kegiatan(Kegiatan_SK) 
+INCLUDE (Jumlah_Pengajuan, Jumlah_Realisasi); 
+GO 
